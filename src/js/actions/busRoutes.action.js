71585909e068174ad9105646047ed8e84be8869d
Vikram Venkataraman <vikramvenkataraman@Vikrams-MacBook-Pro.local>
2018-04-03 10:19:12 -0500

    final update with comments

--- src/js/actions/busRoutes.action.js
+++ src/js/actions/busRoutes.action.js
@@ -2,6 +2,7 @@ import { NEWBUSROUTES, BUSLOCATIONS } from '../constants/counter.contant';
 import  { getVechileCoordinatesObject } from './parsers/busRouteParser';
 import axios from 'axios';
 import store from '../store';
+export const BASE_URL = 'http://webservices.nextbus.com/service/publicJSONFeed?';
 
 export function busRoutes() {
   return {
@@ -12,8 +13,11 @@ export function busRoutes() {
 
 export function buslocations() {
   // :TODO have to move the api functionality to its own directory
-
-    const request = axios.get('http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&r=N&t=1522635676931');
+  // :TODO for each bus route dynamically add for each location.
+  // const date  = new Date();
+  // : TODO have to update date based on time zone fo SFO and pass as parameter.
+  
+    const request = axios.get(`${BASE_URL}command=vehicleLocations&a=sf-muni&r=N&t=1522635676931`);
 
     return(dispatch) => {
       request.then(({data})=> {
